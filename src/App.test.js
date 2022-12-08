1b1ef7e15dba5dddc6ec6143c5b88ae1894fc239
Ezra Mendelson <ezramendelson@Ezras-Mac-mini.local>
2022-12-08 16:55:36 -0500

    implemented new calculation and removed ununsed code

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 import { midnightToEnd } from "./time";
-import {
-  validStartTime,
-  calculatePay,
-  startToMidnight,
-  bedToMidnightDifferenceDollars,
-} from "./time.ts";
+import { validStartTime, calculatePay } from "./time.ts";
 
 test("invalid start time should return false - Cannot start or end before 5:00 PM", () => {
   expect(validStartTime("16:00")).toEqual(false);
@@ -20,71 +15,23 @@ test("Valid Start time should return true", () => {
   expect(validStartTime("18:00")).toEqual(true);
 });
 
-test("If start after midnight pay from start to midnight returns 0 ", () => {
-  expect(startToMidnight("03:00")).toEqual(0);
-});
-
-test("If start at 5 PM pay from start to midnight returns 84 ", () => {
-  expect(startToMidnight("17:00")).toEqual(84);
-});
-
-test("If start at 5:30 PM pay from start to midnight returns 72 ", () => {
-  expect(startToMidnight("17:30")).toEqual(72);
-});
-
-test("If start at 11:00 PM pay from start to midnight returns 12 ", () => {
-  expect(startToMidnight("23:00")).toEqual(12);
-});
-
-test("If bed at 11:00 PM pay from bed to midnight returns 4 ", () => {
-  expect(bedToMidnightDifferenceDollars("23:00")).toEqual(4);
-});
-
-test("If bed at 05:00 PM pay from bed to midnight returns 28", () => {
-  expect(bedToMidnightDifferenceDollars("17:00")).toEqual(28);
-});
-
-test("If end at 11:00 PM pay from midnight to end returns 0", () => {
-  expect(midnightToEnd("23:00", "23:00")).toEqual(0);
-});
-
-test("If end at 01:00 AM pay from midnight to end returns 16", () => {
-  expect(midnightToEnd("23:00", "01:00")).toEqual(16);
-});
-
-test("If end at 04:00 AM pay from midnight to end returns 16", () => {
-  expect(midnightToEnd("23:00", "04:00")).toEqual(64);
-});
-
 test("start at 11:01 end at 12:59 equals 0", () => {
   expect(calculatePay("23:01", "00:59", "00:00")).toEqual(0);
 });
 
 test("max pay should return 148", () => {
-  expect(startToMidnight("17:00")).toEqual(84);
-  expect(bedToMidnightDifferenceDollars("00:00")).toEqual(0);
-  expect(midnightToEnd("17:00", "04:00")).toEqual(64);
-
   expect(calculatePay("17:00", "04:00", "00:00")).toEqual(12 * 7 + 16 * 4);
 });
 
 test("start at 1:00 AM, end at 4:00 AM equals 48", () => {
-  expect(startToMidnight("01:00")).toEqual(0);
-  expect(bedToMidnightDifferenceDollars("00:00")).toEqual(0);
-
   expect(calculatePay("01:00", "04:00", "00:00")).toEqual(48);
 });
 
 test("start at 11:00 PM, end at 4:00 AM equals 76", () => {
-  expect(startToMidnight("23:00")).toEqual(12);
-  expect(midnightToEnd("23:00", "04:00")).toEqual(64);
   expect(calculatePay("23:00", "04:00", "00:00")).toEqual(64 + 12);
 });
 
 test("start at 05:00 PM, bed at 10:00 PM end at 4:00 AM equals 76", () => {
-  expect(startToMidnight("17:00")).toEqual(84);
-  expect(bedToMidnightDifferenceDollars("22:00")).toEqual(8);
-  expect(midnightToEnd("17:00", "04:00")).toEqual(64);
   expect(calculatePay("23:00", "04:00", "00:00")).toEqual(64 + 12);
 });
 
@@ -101,11 +48,11 @@ test("start at 12:00 AM, bed at 10:00 PM throws error", () => {
 });
 
 test("max pay should return 148", () => {
-  expect(startToMidnight("17:00")).toEqual(84);
-  expect(bedToMidnightDifferenceDollars("04:00")).toEqual(0);
-  expect(midnightToEnd("17:00", "04:00")).toEqual(64);
-
   expect(calculatePay("17:00", "04:00", "04:00")).toEqual(12 * 7 + 16 * 4);
+});
+
+test("start at 10:00 PM, bed at 10:00 PM end at 10:00 PM equals 0", () => {
+  expect(calculatePay("22:00", "22:00", "22:00")).toEqual(0);
 });
 
 test("renders learn react link", () => {
--- src/time.ts
+++ src/time.ts
@@ -35,38 +35,38 @@ export function validStartTime(time: String) {
     return converted;
   }
 
-  export function startToMidnight(start: String){
-    if (convertTime(start) < convertTime("04:00")){
-        return 0
-    }
+//   export function startToMidnight(start: String){
+//     if (convertTime(start) < convertTime("04:00")){
+//         return 0
+//     }
 
-    else {
-        return Math.floor((1440 - convertTime(start)) / 60) * 12
-    }
+//     else {
+//         return Math.floor((1440 - convertTime(start)) / 60) * 12
+//     }
 
-  }
+//   }
 
-  export function bedToMidnightDifferenceDollars(bedtime: String) : number{
-    if (convertTime(bedtime) <= convertTime("04:00")){
-        return 0
-    }  
-    else{
-        return Math.floor((1440 - convertTime(bedtime)) / 60) * 4
-    }
-}
+//   export function bedToMidnightDifferenceDollars(bedtime: String) : number{
+//     if (convertTime(bedtime) <= convertTime("04:00")){
+//         return 0
+//     }  
+//     else{
+//         return Math.floor((1440 - convertTime(bedtime)) / 60) * 4
+//     }
+// }
 
-export function midnightToEnd(start: String, end: String){
-    if (convertTime(end) > convertTime("04:00")){
-        return 0
-    }
-    else{
-        if (convertTime(start) < convertTime("03:00")){
-            return Math.floor((convertTime(end) - convertTime(start)) / 60) * 16
-        }
-    }
-    return Math.floor(convertTime(end) / 60) * 16
+// export function midnightToEnd(start: String, end: String){
+//     if (convertTime(end) > convertTime("04:00")){
+//         return 0
+//     }
+//     else{
+//         if (convertTime(start) < convertTime("03:00")){
+//             return Math.floor((convertTime(end) - convertTime(start)) / 60) * 16
+//         }
+//     }
+//     return Math.floor(convertTime(end) / 60) * 16
 
-  }
+//   }
 
 
 
@@ -82,7 +82,37 @@ export function midnightToEnd(start: String, end: String){
     if (!validBedTime(start, end, bedtime)) {
       throw new Error('Invalid bed time - Bed time must be between start and end time')
     }
+    // let calc1 = startToMidnight(start) - bedToMidnightDifferenceDollars(bedtime) + midnightToEnd(start, end)
+    let calc2 = newCalc(start, end, bedtime)
+    return calc2
+  }
 
-    return startToMidnight(start) - bedToMidnightDifferenceDollars(bedtime) + midnightToEnd(start, end)
-    
+  export function newCalc(start: String, end : String, bedtime : String){
+    let c_start = convertTime(start)
+    let c_bed = convertTime(bedtime)
+    let c_end = convertTime(end)
+    let rate = 12
+    let sum = 0
+    let m = {}
+    let j = 0
+    for (let i = 1020; i != 241; i++){
+        m[i] = j
+        j += 1
+        if (i === 1440){
+            i = 0
+        }
+    }
+
+    let x = m[c_start]
+    while (x <= m[c_end]-60){
+        if (x >= m[c_bed]){
+            rate = 8
+        }
+        if (x >= 420){
+            rate = 16
+        }
+        sum += rate
+        x += 60
+    }
+    return sum
   }
